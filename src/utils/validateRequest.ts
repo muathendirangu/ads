e3f222f4a8471bb862d2d053dcd00dfe796d4b23
muathendirangu <muathe.ndirangu@gmail.com>
2023-10-18 02:03:45 +0300

    add validation functions for the request body

---
+++ src/utils/validateRequest.ts
@@ -0,0 +1,17 @@
+
+ export function isArray(array: any[]): boolean {
+    return Array.isArray(array);
+ }
+
+ export function isEmpty(array: any[]): boolean {
+    return array.length === 0;
+ }
+ export function containsEmptyString(array: any[]): boolean {
+    return array.some((item) => item === '');
+ }
+
+ export function containsNon42CharacterString(array: any[]): boolean {
+    return array.some((item) => item.length !== 42);
+ }
+
+
